a6383526ee3c7c338be8415a24cf0ec1891435c3
beryli <41893853+beryli@users.noreply.github.com>
2018-08-19 16:30:53 +0800

    Delete test.php

--- Forum/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include ('dbconn.php');
-	$select = mysqli_query($conn,"select * from categories"); # $conn是來自dbconn.php
-	while ($row = mysqli_fetch_assoc($select)) {
-		# code...
-		echo "<h1>".$row['cat_title'],"</h1>";
-	}
-?>
